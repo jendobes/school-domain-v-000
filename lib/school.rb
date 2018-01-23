6df72795f53398852b30fe25e368e73530078ecb
Jennifer Dobak <jdobak1@gmail.com>
2018-01-23 17:38:53 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -24,7 +24,15 @@ class School
     end
     sorted
   end
-
-
 end
+
+@school = School.new
+
+@school.add_student("Homer Simpson", 9)
+@school.add_student("Bart Simpson", 9)
+@school.add_student("Avi Flombaum", 10)
+@school.add_student("Jeff Baird", 10)
+@school.add_student("Blake Johnson", 7)
+@school.add_student("Jack Bauer", 7)
+
 binding.pry
