7404aba1a7d148bba59ff5724b550fbe0658cfbd
Jennifer Dobak <jdobak1@gmail.com>
2018-01-23 19:20:36 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,4 @@
 # code here!
-require 'pry'
 class School
 
   attr_reader :roster
@@ -18,23 +17,10 @@ class School
   end
 
   def sort
-=begin  sorted = {}
+ sorted = {}
     @roster.each do |grade, students|
       sorted[grade] = students.sort
     end
     sorted
   end
-=end
-  sorted = Hash[@roster.sort_by {|grade, student| student}]
 end
-
-@school = School.new("playschool")
-
-@school.add_student("Homer Simpson", 9)
-@school.add_student("Bart Simpson", 9)
-@school.add_student("Avi Flombaum", 10)
-@school.add_student("Jeff Baird", 10)
-@school.add_student("Blake Johnson", 7)
-@school.add_student("Jack Bauer", 7)
-
-binding.pry
